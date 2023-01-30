ffb7c4b2319d4ac681c52de3e5d186dee71aa2c2
btoschek <btoschek@protonmail.com>
2023-01-30 19:38:38 +0100

    nvim(telescope): Borderless windows

--- .config/nvim/lua/btoschek/plugins/_lazy.lua
+++ .config/nvim/lua/btoschek/plugins/_lazy.lua
@@ -31,9 +31,7 @@ local plugins = {
     'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
-    config = function()
-      vim.cmd('colorscheme tokyonight-night')
-    end,
+    config = get_config('tokyonight'),
   },
 
   {
