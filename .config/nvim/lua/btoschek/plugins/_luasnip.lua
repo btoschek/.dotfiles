ac604323e8c43d8b6d5f041340b2ee7c41f20386
btoschek <btoschek@protonmail.com>
2023-04-05 23:55:29 +0200

    nvim(luasnip): Remove snippet jump marks on mode change

--- .config/nvim/lua/btoschek/plugins/_luasnip.lua
+++ .config/nvim/lua/btoschek/plugins/_luasnip.lua
@@ -19,3 +19,18 @@ luasnip.config.setup {
 require('luasnip.loaders.from_lua').load({
   paths = vim.fn.stdpath('config') .. '/snippets'
 })
+
+-- Stop snippets when leaving insert/substitute mode
+-- (Based on: https://github.com/L3MON4D3/LuaSnip/issues/258)
+vim.api.nvim_create_autocmd('ModeChanged', {
+  group = vim.api.nvim_create_augroup('btoschek-luasnip', { clear = true }),
+  callback = function()
+    if
+      ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+      and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+      and not require('luasnip').session.jump_active
+    then
+      require('luasnip').unlink_current()
+    end
+  end
+})
