d3e03aedd5b6cc8d3e5b2e73b69a6f3b193fe65f
btoschek <btoschek@protonmail.com>
2023-05-22 16:46:48 +0200

    nvim(bufferline): Don't show oil.nvim buffers in bufferline

--- .config/nvim/lua/btoschek/plugins/_bufferline.lua
+++ .config/nvim/lua/btoschek/plugins/_bufferline.lua
@@ -1,1 +1,9 @@
-require('bufferline').setup {}
+require('bufferline').setup {
+  options = {
+    custom_filter = function(buf_number, buf_numbers)
+      if vim.fn.bufname(buf_number):find('^oil://') == nil then
+        return true
+      end
+    end,
+  },
+}
