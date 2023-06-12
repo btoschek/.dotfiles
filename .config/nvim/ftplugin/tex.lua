3b4407e8605567ed0b0f9855ba66be14aa282e40
btoschek <btoschek@protonmail.com>
2023-06-12 12:39:19 +0200

    nvim(ftplugin): Add german spellchecking to .tex files

--- .config/nvim/ftplugin/tex.lua
+++ .config/nvim/ftplugin/tex.lua
@@ -1,3 +1,9 @@
 local opt = vim.opt
 
 opt.conceallevel = 2                      -- Conceal some markers
+
+-- Enable spellchecking for German
+vim.cmd [[
+  setlocal spell spelllang=de
+  set spell
+]]
