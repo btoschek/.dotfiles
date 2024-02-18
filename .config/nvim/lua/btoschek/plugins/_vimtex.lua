d7fc6cfb01b17fac65c490c14c37b0332c328f0f
btoschek <btoschek@protonmail.com>
2024-02-18 13:02:12 +0100

    nvim(vimtex): Make latexmk use hidden build directory

--- .config/nvim/lua/btoschek/plugins/_vimtex.lua
+++ .config/nvim/lua/btoschek/plugins/_vimtex.lua
@@ -2,3 +2,6 @@ vim.g.tex_flavor = 'latex'
 vim.g.tex_conceal = 'abdmg'
 vim.g.vimtex_view_method = 'zathura'
 vim.g.vimtex_quickfix_mode = 0
+vim.g.vimtex_compiler_latexmk = {
+  out_dir = ".build", -- Use hidden directory to build the document
+}
