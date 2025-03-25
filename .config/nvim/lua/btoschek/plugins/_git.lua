d53f33e9f810d3a534c79ee38da56e4c6eaa600b
btoschek <btoschek@protonmail.com>
2025-03-25 22:43:02 +0100

    nvim(git): Fix gitsigns warnings & remove lazygit in toggleterm

--- .config/nvim/lua/btoschek/plugins/_git.lua
+++ .config/nvim/lua/btoschek/plugins/_git.lua
@@ -1,10 +1,10 @@
 require('gitsigns').setup {
   signs = {
-    add          = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'dark' },
-    change       = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-    delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-    topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-    changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+    add          = { text = '│' },
+    change       = { text = '│' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
   },
   current_line_blame = true, -- NOTE: Search for option to display after rust-tools inlay hints
 }
@@ -17,24 +17,3 @@ vim.cmd [[
   highlight GitGutterChangeDelete guibg=dark
   highlight GitGutterDelete guibg=dark
 ]]
-
--- Open a toggleterm with lazygit
-function Lazygit_toggle()
-  local term_command = 'lazygit'
-
-  -- Open dotfiles if not in git directory
-  if not require('galaxyline.condition').check_git_workspace() then
-    term_command = term_command .. ' --git-dir=' .. vim.env.HOME .. '/.dotfiles/ --work-tree=' .. vim.env.HOME .. '/'
-  end
-
-  local terminal = require('toggleterm.terminal').Terminal
-  local lazygit  = terminal:new({
-    cmd = term_command,
-    direction = 'float',
-    hidden = true,
-  })
-
-  lazygit:toggle()
-end
-
-vim.keymap.set('n', '<Leader>gg', Lazygit_toggle, { desc = 'Open lazygit', noremap = true, silent = true })
